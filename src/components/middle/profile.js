b5918c142af4b835ae4fb86c2972cd495c29ec73
Matheus.R <matheus.rlank2@gmail.com>
2021-09-19 22:57:35 -0300

    VS-F

--- src/components/middle/profile.js
+++ src/components/middle/profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
     return(
         <section className="profile">
             <div className="profile-background">
-                <img src="https://avatars.githubusercontent.com/u/75798474?v=4" alt=" " className="photo"/>
+                <img src="https://avatars.githubusercontent.com/u/75798474?v=4" alt="perfil" className="photo"/>
                 <h1 className="h1">Matheus Rocha Lã</h1>
                 <h2 className="h2">Front-end Developer</h2>
             </div>
